a01f10de22bd4c78f58326586c228207fa444f84
suzannenl <skr470@gmail.com>
2022-01-04 21:43:08 +0100

    Create functionality for adding items to cart context (incl. useRef and forwardRef)

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = (props, ref) => {
   return <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
       {/*{...props.input} ensures that all the key value pairs in the input object are added as props to the input */}
-      <input {...props.input}/>
+      <input ref={ref} {...props.input}/>
   </div>;
 };
 
-export default Input;
+export default React.forwardRef(Input);
