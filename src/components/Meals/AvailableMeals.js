a69db53955ac2fdb2429d3ee8f71842961a0e5bf
suzannenl <skr470@gmail.com>
2022-01-13 18:45:38 +0100

    Handle errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -11,12 +11,19 @@ const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   // We always start with loading data when the component is rendered, therefore we set this to true:
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     // The function passed to useEffect should not return a promise, it must not be turned into an async function.
     // Therefore I write a separate async fetchMeals function, which I execute as part of useEffect.
     const fetchMeals = async () => {
       const response = await fetch(`${REACT_APP_FB_URL}meals.json`);
+
+      // If this happens, we move to 'catch'
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       // Parse data:
       const responseData = await response.json();
       // The values for the meal keys are nested properties. So responseData is an object, which I then transform into an array:
@@ -34,13 +41,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
