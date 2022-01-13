f7bb8f4c0e48f81f950d3984b18eca73c2f0ec47
suzannenl <skr470@gmail.com>
2022-01-13 21:19:56 +0100

    Clear cart after sending order

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -57,6 +57,12 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  // Empty cart after submitting the checkout form.
+  if (action.type === "CLEAR") {
+    return defaultCartState;
+  }
+
   return defaultCartState;
 };
 
@@ -77,12 +83,18 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: "CLEAR" });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
